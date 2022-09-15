cafb2f8bdc8d0104c916e0974c971bc9b25f3987
shearichard <rshea@thecubagroup.com>
2022-09-15 16:04:10 +1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+This script isn't mean to do very much other than serve as a test
+bed for using pip/requirements.txt
+'''
+
+
+def main():
+    print("If I did any less I wouldn't even be a thing")
+
+
+if __name__ == "__main__":
+    main()
